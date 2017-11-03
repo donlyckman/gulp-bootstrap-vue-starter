87727bba4842870e5abc19d5475a9ce6811ebeff
donlyckman <donlyckman@sportngin.com>
2017-11-03 17:47:11 -0500

    Updates to handle Bootstrap, jQuery and popper libraries

--- gulpfile.js
+++ gulpfile.js
@@ -34,11 +34,21 @@ gulp.task( 'html', function() {
         .pipe( gulp.dest( dst ) ); // write files
 } );
 
-[ 'src/scss/*.scss'];
+// Task for vendors JS files
+gulp.task( 'vendorsJS', function () {
+    return gulp.src( ['node_modules/jquery/dist/jquery.slim.min.js', 'node_modules/popper.js/dist/umd/popper.min.js', 'node_modules/bootstrap/dist/js/bootstrap.min.js'] )
+        .pipe( gulp.dest( dst + '/js' ) );
+} );
+
+// Task for vendors CSS files
+gulp.task( 'vendorsCSS', function () {
+    return gulp.src( ['node_modules/bootstrap/dist/css/bootstrap.min.css', 'node_modules/bootstrap/dist/css/bootstrap.min.css.map'] )
+        .pipe( gulp.dest( dst + '/css' ) );
+} );
 
 // Task for SASS Files 
 gulp.task( 'sass', function() {
-    return gulp.src( ['node_modules/bootstrap/scss/bootstrap.scss', src + '/scss/**/*.scss'] )
+    return gulp.src( src + '/scss/**/*.scss' )
         .pipe( sass() ) // gulp-sass
         .pipe( sourcemaps.init() )
         .pipe( autoprefixer( 'last 2 version' ) ) // gulp-autoprefixer
@@ -52,7 +62,7 @@ gulp.task( 'sass', function() {
 
 // Task for JS Files
 gulp.task( 'scripts', function() {
-    return gulp.src( ['node_modules/jquery/dist/jquery.slim.min.js', 'node_modules/popper.js/dist/umd/popper.min.js', 'node_modules/bootstrap/dist/js/bootstrap.min.js', src + '/js/**/*.js'] )
+    return gulp.src( src + '/js/**/*.js' )
         .pipe( sourcemaps.init() )
         .pipe( stripDebug() )
         .pipe( uglify() )
@@ -91,7 +101,7 @@ gulp.task( 'clean', function( done ) {
 
 // Task for Build
 gulp.task( 'build', function( done ) {
-    run_sequence( 'clean', ['html', 'images', 'scripts', 'sass'], done );
+    run_sequence( 'clean', ['html', 'images', 'scripts', 'sass', 'vendorsJS', 'vendorsCSS'], done );
 } );
 
 // Task for Loading Browsersync and watching for project changes
